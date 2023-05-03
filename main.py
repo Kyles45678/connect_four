68a82f5655d95a3c1eb394e6d3a5eb6457b002a1
otteliena <otteliena@msoe.edu>
2023-05-03 08:01:28 -0500

    analysis done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from agent import Agent
 from board import Board
 from random_pattern import RandomPattern
+import time
 
 COLUMNS = 7
 ROWS = 6
@@ -35,41 +36,53 @@ def board_can_be_played(board):
 
 
 def run_game(board, agent1, agent2):
+    turns = 0
+    winner = None
+    start = time.perf_counter()
     print("---------------- STARTING GAME ----------------")
     board.print_board()
 
     current_player = agent1
     while board_can_be_played(board):
+        turns += 1
         print(str(current_player) + "'s turn!")
         piece = current_player.play_move()
         board.print_board()
-        # print(board_has_winner(piece, board))
         if board_has_winner(piece, board):
-            print("We have a winner!")
+            print("We have a winner! Its " + str(current_player) + "!")
+            winner = str(current_player)
             break
         elif not board_can_be_played(board):
             print("Stalemate! WAAHHHH!!!!")
+            winner = "Stalemate"
             break
 
-        # board.print_board()
         print("-------- turn over --------")
         if current_player == agent1:
             current_player = agent2
         else:
             current_player = agent1
-
-    # if board_has_winner(board):
-    #     print("We have a winner!")
-    # elif not board_can_be_played(board):
-    #     print("Stalemate! WAAHHHH!!!!")
+    game_time = time.perf_counter() - start
+    return [game_time, turns, winner]
 
 
 def main():
-    board = Board(COLUMNS, ROWS)
-    agent1 = Agent(board, RandomPattern(board), "Yellow")
-    agent2 = Agent(board, RandomPattern(board), "Red")
+    stats = [0, 0, [0, 0, 0]]
+    for i in range(100):
+        board = Board(COLUMNS, ROWS)
+        agent1 = Agent(board, RandomPattern(board), "Yellow")
+        agent2 = Agent(board, RandomPattern(board), "Red")
 
-    run_game(board, agent1, agent2)
+        game_stats = run_game(board, agent1, agent2)
+        stats[0] += game_stats[0]
+        stats[1] += game_stats[1]
+        if game_stats[2] == "Yellow":
+            stats[2][0] += 1
+        elif game_stats[2] == "Red":
+            stats[2][1] += 1
+        else:
+            stats[2][2] += 1
+        print(stats)
 
 
 main()
---
+++ smart_pattern.py
@@ -0,0 +1,26 @@
+
+# from piece import Piece
+
+#
+# class SmartPattern:
+#
+#     def __init__(self, board):
+#         self.board = board
+#
+#     def act(self, agent):
+#         playable_columns = []
+#         for c in range(self.board.width):
+#             if not self.board.is_column_full(c):
+#                 playable_columns.append(c)
+#
+#         if len(playable_columns) > 0:
+            # chosen_col = random.choice(playable_columns)
+            # piece = Piece(0, 0, agent.name)
+            # (placed_col, placed_row) = self.board.place_piece(chosen_col, piece)
+            # if placed_row == -1:
+            #     return None
+            # piece.set_pos(placed_col, placed_row)
+            # return piece
+        # else:
+        #     return None
+
